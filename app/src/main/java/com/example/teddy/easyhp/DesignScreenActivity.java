aa28a7761d229d67669c804ea6d2e3cbf22c58bd
forteddyt <forteddyt@gmail.com>
2016-10-01 00:02:04 -0400

    No Actionbar DisplayScreen

---
+++ app/src/main/java/com/example/teddy/easyhp/DesignScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.teddy.easyhp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class DesignScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.design_screen);
+    }
+   }
